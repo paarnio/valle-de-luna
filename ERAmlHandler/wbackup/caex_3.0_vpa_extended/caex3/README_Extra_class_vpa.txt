d0f9e0f2d2623becf9b9cce0c3be6dc5487f8221
paarnio <p.aarnio@gmail.com>
2017-08-30 10:33:56 +0300

    WPC: New CAEX 3.0 jaxb files added to siima.models.jaxb.caex3 package

---
+++ ERAmlHandler/wbackup/caex_3.0_vpa_extended/caex3/README_Extra_class_vpa.txt
@@ -0,0 +1,7 @@
+Original java classes genereted from schema: CAEX_ClassModel_V3.0_byVPA.xsd 
+
+ADD: AppInfoEXTRAContentType.java (containing WriterHeader inner class)
+and ko. object creations added also to ObjectFactory
+
+PLUS:
+TEMP_Helpper.java
---
+++ ERAmlHandler/wbackup/caex_3.0_vpa_extended/caex3/TEMP_Helpper.java
@@ -0,0 +1,15 @@
+package siima.models.jaxb.caex3;
+
+/* --- Note package
+ * This needs to be in jaxb model package, because 
+ * newbasic.additionalInformation is a protected property
+ * containing no setter method.
+ */
+ 
+public class TEMP_Helpper {
+	public static CAEXBasicObject insertCopyContent(CAEXBasicObject original){	
+		CAEXBasicObject newbasic = new CAEXBasicObject();
+		newbasic.additionalInformation = original.getAdditionalInformation();		
+		return newbasic;
+	}
+}
